12fc323b4cafef364bfcb8d74c19fcae52d2f2ed
nombre15 <81451283+nombre15@users.noreply.github.com>
2021-05-25 22:45:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,221 +3,220 @@
 #include <string>
 #include <chrono>
 #include <thread>
+#include <windows.h>
 using namespace std;
 
-void NUMBER (int);
+void NUMERO_AZAR(int);
+void ELEGIR_NUMERO_AZAR(int);
 
 int main()
 {
-    string bet;
-    int BetNumber;
-    cout << "como quieres apostar?" << endl;
-    cout << " impares \n pares \n rojos \n negros \n un solo numero \n numero del 1 - 18 \n numero del 19 - 36" << endl;
-    cin >> bet;
-
-    single:
-    if(bet == "numero"){
-
-    cout << "escribe un numero para apostar" << endl;
-    cin >> BetNumber;
-    if(BetNumber > 37){
-        cout << "ese no es un numero valido" << endl;
-        goto single;
-    }
-
+    SetConsoleTitle("Ruleta");
+    string apuesta;
+    int NumeroApuesta;
     srand(time(0));
+    int numero = rand() % 37;
 
-    cout <<"girando..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
+    cout << "Como quieres apostar?" << endl;
+    cout << "\nImpares \nPares \nRojos \nNegros \nUn numero \nNumero del 1 - 18 \nNumero del 19 - 36\n" << endl;
 
-        int number = rand() % 37;
-        if(number == BetNumber){
+    apostar:
+    cin >> apuesta;
 
-            cout << "\nganaste!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
+    solo:
+    if(apuesta == "numero" || apuesta == "Numero"){
 
+    cout << "Escribe el numero con el que quieres apostar" << endl;
+    cin >> NumeroApuesta;
+    if(NumeroApuesta > 37){
+        cout << "Ese no es un numero valido" << endl;
+        goto solo;
+    }
+
+    ELEGIR_NUMERO_AZAR(numero);
+
+    if(numero == NumeroApuesta){
+
+        cout << "\nGanaste!" << endl;
+        cout << "El numero era ";
+        NUMERO_AZAR(numero);
+
+  }
+    else if(numero != NumeroApuesta){
+
+        cout << "\nPerdiste!" << endl;
+        cout << "El numero era ";
+        NUMERO_AZAR(numero);
+     }
+  }
+
+    if(apuesta == "impares" || apuesta == "impar" || apuesta == "Impar" || apuesta == "Impares"){
+
+        ELEGIR_NUMERO_AZAR(numero);
+
+        if (numero % 2 != 0){
+
+            cout << "\nGanador!" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
         }
-        else if(number != BetNumber){
+        else{
 
-            cout << "\nperdiste!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
+            cout << "\nPerdedorrrr!!" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+  }
+     if(apuesta == "pares" || apuesta == "par" || apuesta == "Pares" || apuesta == "Par"){
+
+       ELEGIR_NUMERO_AZAR(numero);
+
+        if (numero % 2 == 0){
+
+            cout << "\nGanador!" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+        else{
+
+            cout << "\nperdedorr!!" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+  }
+     if(apuesta == "rojo" || apuesta == "rojos" || apuesta == "Rojo" || apuesta == "Rojos"){
+
+        ELEGIR_NUMERO_AZAR(numero);
+
+        if (numero % 2 == 0  && ((11 < numero && numero < 19) || (29 < numero && numero < 37)))
+        {
+            cout << "\nGanador!!" <<endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+        else if (numero % 2 != 0 && ((0 < numero && numero < 10) || (18 < numero && numero < 28))){
+
+            cout << "\nGanador!!" <<endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+        else{
+
+           cout << "\nPerdisteeee" << endl;
+           cout << "El numero era ";
+           NUMERO_AZAR(numero);
+        }
+  }
+     if(apuesta == "negro" || apuesta == "negros" || apuesta == "Negro" || apuesta == "Negros"){
+
+        ELEGIR_NUMERO_AZAR(numero);
+
+       if (numero % 2 == 0 && ((0 < numero && numero < 11) || (19 < numero && numero < 29)))
+        {
+            cout << "\nGanador ganador" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+     }
+       else if(numero % 2 != 0 && ((10 < numero && numero < 18) || (28 < numero && numero < 36)))
+        {
+           cout << "\nGanador" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+      else{
+
+        cout << "\nPerdisteee" << endl;
+           cout << "El numero era ";
+           NUMERO_AZAR(numero);
+        }
+  }
+
+    if(apuesta == "1-18" || apuesta == "numero del 1 - 18" || apuesta == "1al18" || apuesta == "Numero del 1 al 18"){
+
+        ELEGIR_NUMERO_AZAR(numero);
+
+        if(0 < numero && numero < 19){
+
+            cout << "\nGanaste!" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+      else{
+
+            cout << "\nPerdiste!" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+  }
+    if(apuesta == "19-36" || apuesta == "numero del 19 - 36" || apuesta == "19al36" || apuesta == "numero del 19 al 36"){
+
+        ELEGIR_NUMERO_AZAR(numero);
+
+        if(18 < numero && numero < 37){
+
+            cout << "\nGanaste!" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
+        }
+        else{
+
+            cout << "\nPerdiste!" << endl;
+            cout << "El numero era ";
+            NUMERO_AZAR(numero);
         }
     }
 
-     if(bet == "impares" || bet == "impar"){
-
-        srand(time(0));
-        cout <<"girando..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
-
-        if (number % 2 != 0){
-
-            cout << "\nganador!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-        else{
-
-            cout << "\nperdedorrr!!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-     }
-     if(bet == "pares" || bet == "par"){
-
-        srand(time(0));
-        cout <<"girando..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
-
-        if (number % 2 == 0){
-
-            cout << "\nganador!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-        else{
-
-            cout << "\nperdedorrr!!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-     }
-     if(bet == "rojo" || bet = "rojos"){
-
-        srand(time(0));
-        cout <<"girando..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
-
-        if (number % 2 == 0  && ((11 < number && number < 19) || (29 < number && number < 37)))
-        {
-            cout << "\nganador!!" <<endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-        else if (number % 2 != 0 && ((0 < number && number < 10) || (18 < number && number < 28))){
-
-            cout << "\nganador!!" <<endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-        else{
-
-           cout << "\nperdistee" << endl;
-           cout << "el numero era ";
-           NUMBER(number);
-        }
-     }
-     if(bet == "negros" || bet == "negro" ){
-
-        srand(time(0));
-        cout <<"girando..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
-
-      if (number % 2 == 0 && ((0 < number && number < 11) || (19 < number && number < 29)))
-        {
-            cout << "\nganador ganador" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-     }
-       else if(number % 2 != 0 && ((10 < number && number < 18) || (28 < number && number < 36)))
-        {
-           cout << "\nganador ganador" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
     else{
 
-        cout << "\nperdistee" << endl;
-           cout << "el numero era ";
-           NUMBER(number);
-        }
+        cout << "\nJuega otra vez!\n" << endl;
+        goto apostar;
     }
-
-    if(bet == "1-18" || bet == "numero del 1 - 18" || bet == "1al18" || bet == "numero del 1 al 18"){
-
-        srand(time(0));
-        cout <<"girando..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
-
-        if(0 < number && number < 19){
-
-            cout << "\nganaste!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-        else{
-
-            cout << "\nperdiste!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-    }
-    if(bet == "19-36" || bet == "numero del 19 - 36" || bet == "19al36" || bet == "numero del 19 al 36"){
-
-        srand(time(0));
-        cout <<"girando..." << endl;
-    this_thread::sleep_for (chrono::seconds(2));
-        int number = rand() % 37;
-
-        if(18 < number && number < 37){
-
-            cout << "\nganaste!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-        else{
-
-            cout << "\nperdiste!" << endl;
-            cout << "el numero era ";
-            NUMBER(number);
-        }
-    }
-
 }
 
-void NUMBER(int number){
- if(number == 0)
+//elegir color de acuerdo al numero
+void NUMERO_AZAR(int numero){
+
+ if(numero == 0)
+{
+    cout << numero << " verde" << endl; //GREEN
+}
+
+        else if (numero % 2 == 0 && ((0 < numero && numero < 11) || (19 < numero && numero < 29))) //even blacks (1 -> 10 & 20 -> 28)
         {
-            cout << number << " verde" << endl;
+            cout << numero << " negro" << endl;
+
         }
 
-        /*even blacks (1 -> 10 & 20 -> 28)*/
-        else if (number % 2 == 0 && ((0 < number && number < 11) || (19 < number && number < 29)))
+        else if (numero % 2 == 0 && ((11 < numero && numero < 19) || (29 < numero && numero < 37))) //even reds (12 -> 18 & 30 -> 36)
         {
-            cout << number << " negro" << endl;
-
+            cout << numero << " rojo" << endl;
         }
 
-        /*even reds (12 -> 18 & 30 -> 36)*/
-        else if (number % 2 == 0 && ((11 < number && number < 19) || (29 < number && number < 37)))
+        else if (numero % 2 != 0 && ((0 < numero && numero < 10) || (18 < numero && numero < 28))) //odd reds (1 -> 9 & 19 -> 27)
         {
-            cout << number << " rojo" << endl;
+            cout << numero << " rojo" << endl;
         }
 
-        /*odd reds (1 -> 9 & 19 -> 27)*/
-        else if (number % 2 != 0 && ((0 < number && number < 10) || (18 < number && number < 28)))
+        else if (numero % 2 != 0 && ((10 < numero && numero < 18) || (28 < numero && numero < 36))) //odd blacks (11 -> 17 & 29 -> 35)
         {
-            cout << number << " rojo" << endl;
+            cout << numero << " negro" << endl;
         }
 
-        /*odd blacks (11 -> 17 & 29 -> 35)*/
-        else if (number % 2 != 0 && ((10 < number && number < 18) || (28 < number && number < 36)))
-        {
-            cout << number << " negro" << endl;
-        }
-
-        /*error?*/
         else
         {
-            cout << number << "error???" << endl;
+            cout << numero << "ERROR???" << endl; //error?
         }
 }
 
+//elegir numero al azar
+void ELEGIR_NUMERO_AZAR(int numero){
 
+     cout <<"Girando..." << "\n" << endl;
+
+     for(int x = 0; x < 8; x++){
+
+        numero = rand() % 37;
+        this_thread::sleep_for (chrono::milliseconds(500));
+        cout << numero << "\r";
+     }
+}
